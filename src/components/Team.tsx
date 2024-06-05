d1826b929494a0ad6ffead9f1f8e503704f3e135
Silja <silja.rheingans@innogames.com>
2024-06-05 17:32:15 +0200

    adjust team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -20,60 +20,65 @@ const Team: React.FC<TeamProps> = (props) => {
             justifyContent='center' 
             sx={{backgroundColor: 'secondary.light', padding: '50px' }}
         >
-            {teamMembers.map((member, index) => (
-                <Grid key={index}>
-                    <TeamCard teamMember={member}/>
-                    </Grid>
-                ))
-            }
             <Title title={'Das Team'}/>
             <Grid
-                sx={{
-                    display: 'flex', 
-                    flexDirection: {
-                        lg: 'row',
-                        xs: 'column',
-                    },
-                    justifyContent: 'space-evenly', 
-                    alignItems: 'center'
-                }}
+                container 
+                justifyContent='center' 
             >
-                <Box
+                {teamMembers.map((member, index) => (
+                    <Grid key={index}>
+                        <TeamCard teamMember={member}/>
+                        </Grid>
+                    ))
+                }
+                <Grid
                     sx={{
-                        width: {
-                            lg: '70%',
-                            xs: '95%',
-                                },
-                        height: 'auto',
-                        margin: '10px',               
+                        display: 'flex', 
+                        flexDirection: {
+                            lg: 'row',
+                            xs: 'column',
+                        },
+                        justifyContent: 'space-evenly', 
+                        alignItems: 'center'
                     }}
                 >
-                    <img
-                        src='Locations/Logo_Evangelische_Kirche_Gemeinde_Wannsee.png'
-                        style={{
-                            width: '100%', 
-                            height: 'auto'
+                    <Box
+                        sx={{
+                            width: {
+                                lg: '70%',
+                                xs: '95%',
+                                    },
+                            height: 'auto',
+                            margin: '10px',               
                         }}
-                    />
-                </Box>
-                <Box
-                    sx={{
-                        width: {
-                            lg: '70%',
-                            xs: '95%',
-                                },
-                        height: 'auto',
-                        margin: '10px',               
-                    }}
-                >
-                    <img
-                        src='Locations/Logo_Nikolskoe.png'
-                        style={{
-                            width: '100%', 
-                            height: 'auto'
+                    >
+                        <img
+                            src='Locations/Logo_Evangelische_Kirche_Gemeinde_Wannsee.png'
+                            style={{
+                                width: '100%', 
+                                height: 'auto'
+                            }}
+                        />
+                    </Box>
+                    <Box
+                        sx={{
+                            width: {
+                                lg: '70%',
+                                xs: '95%',
+                                    },
+                            height: 'auto',
+                            margin: '10px',               
                         }}
-                    />
-                </Box>
+                    >
+                        <img
+                            src='Locations/Logo_Nikolskoe.png'
+                            style={{
+                                width: '100%', 
+                                height: 'auto'
+                            }}
+                        />
+                    </Box>
+                </Grid>
             </Grid>
         </Grid>
     );
