b5968a2caee26bf0fcbfb1acce2cbf69cde7a128
Silja <silja.rheingans@innogames.com>
2024-06-05 15:30:25 +0200

    adjust iframe

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -116,7 +116,7 @@ const Locations: React.FC = () => {
                         maxWidth: '95%',
                         width: {
                             lg: '70%',
-                            xs: '900%',
+                            xs: '90%',
                                 },
                         height: 'auto',
                         margin: '2.5%',
@@ -126,7 +126,7 @@ const Locations: React.FC = () => {
                 >
                     <iframe
                         src={'https://www.google.com/maps/embed?pb=!1m40!1m12!1m3!1d11263.185444849798!2d13.127066847415565!3d52.42058421961294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m25!3e2!4m5!1s0x47a8589f70e949a1%3A0xf470b8b3e9c07fcb!2sEv.%20Kirche%20St.%20Peter%20und%20Paul%20auf%20Nikolskoe%2C%20Nikolskoer%20Weg%2C%20Berlin!3m2!1d52.4247097!2d13.1177108!4m5!1s0x47a859df624f8189%3A0x3d2bfd380a19a06f!2sAndreaskirche%20-%20Ev.%20Kirchengemeinde%20Berlin-Wannsee%2C%20Lindenstra%C3%9Fe%203%2C%2014109%20Berlin!3m2!1d52.4252432!2d13.1559449!4m5!1s0x47a858c7b4c80d89%3A0xf69cf2746c37a06a!2sKirche%20am%20St%C3%B6lpchensee%20-%20Ev.%20Kirchengemeinde%20Berlin-Wannsee%2C%20Wilhelmplatz%2C%20Berlin!3m2!1d52.411975!2d13.141425!4m5!1s0x47a8589f70e949a1%3A0xf470b8b3e9c07fcb!2sSs.%20Peter%20and%20Paul%2C%20Wannsee%2C%20Westlicher%20D%C3%BCppeler%20Forst%2C%20Hundeauslaufgebiet%20Wannsee-D%C3%BCppel%2C%20Nikolskoer%20Weg%2C%20Berlin!3m2!1d52.4247097!2d13.1177108!5e0!3m2!1sen!2sde!4v1716990871644!5m2!1sen!2sde'}
-                        width='100%'
+                        width='90%'
                         height='450'
                         style={{ 
                             border: 0,
